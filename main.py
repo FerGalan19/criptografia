3cd748224141bfd3289a2973685ca76626939ec8
FerGalan19 <99019697+FerGalan19@users.noreply.github.com>
2022-10-17 13:43:46 +0200

    contraseña

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
+import os
 from cryptography.fernet import Fernet
 key = Fernet.generate_key()
 f = Fernet(key)
 token = f.encrypt(b"A really secret message. Not for prying eyes.")
 token
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+salt = os.urandom(16)
+print(salt)
+# derive
+kdf = Scrypt(
+    salt=salt,
+    length=32,
+    n=2**14,
+    r=8,
+    p=1,
+    )
+key = kdf.derive(b"my great password")
+# verify
+kdf = Scrypt(
+    salt=salt,
+    length=32,
+    n=2**14,
+    r=8,
+    p=1,
+    )
+kdf.verify(b"my great password", key)
