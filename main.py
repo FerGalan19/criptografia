f74ed7cd808dfedc7b4739f0b5944b5f7412327f
100451179 <100451179@alumnos.uc3m.es>
2022-10-17 13:54:19 +0200

    Creación menú en el main

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
-import os
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-f = Fernet(key)
-token = f.encrypt(b"A really secret message. Not for prying eyes.")
-token
+import time
+from bank_system import function
 
-from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
-salt = os.urandom(16)
-print(salt)
-# derive
-kdf = Scrypt(
-    salt=salt,
-    length=32,
-    n=2**14,
-    r=8,
-    p=1,
-    )
-key = kdf.derive(b"my great password")
-# verify
-kdf = Scrypt(
-    salt=salt,
-    length=32,
-    n=2**14,
-    r=8,
-    p=1,
-    )
-kdf.verify(b"my great password", key)
+Function = function.Function()
+
+class Main(object):
+    def run(self):
+        while True:
+            print("Saltando a la interfaz principal, por favor espere", end="")
+            for i in range(5):
+                print(".", end="", flush=True)
+                time.sleep(0.4)
+            print("")
+            self.UI()
+            number = input("Ingrese el número de función que desea seleccionar:")
+            if number not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'q']:
+                print("¡Ingrese el código correcto!")
+            if number == "0":
+                Function.create_user()
+            elif number == "1":
+                Function.query()
+            elif number == "2":
+                Function.deposit()
+            elif number == "3":
+                Function.withdrawal()
+            elif number == "4":
+                Function.change_password()
+            elif number == "q":
+                print("Saliendo del sistema, por favor espere", end=" ")
+                for i in range(6):
+                    print(".", end=" ", flush=True)
+                    time.sleep(0.5)
+                break
+
+    def UI(self):
+        print("*****************************************************")
+        print("** Bienvenido a - Col china - China Merchants Bank **")
+        print("** Consulta de apertura de cuenta (0) (1) **")
+        print("** Depósito (2) Retiro (3) **")
+        print("** Cambiar contraseña (5) **")
+        print("** Salir (q) **")
+        print("*****************************************************")
+
+if __name__ == "__main__":
+    Main().run()
